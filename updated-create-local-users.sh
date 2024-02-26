79f6910a0751a7a89a5971d089262f5c9d39121e
chno <chnoinfosec@gmail.com>
2024-02-25 22:05:41 -0500

    [IMP] Improved on this script for better error handling including more in depth error echoing as well as standard error additions.

--- updated-create-local-users.sh
+++ updated-create-local-users.sh
@@ -1,67 +1,73 @@
 #!/bin/bash
 
-# A script that creates user accounts, sets passwords, and handles errors if the user creation fails.
-# Checks whether the script is being executed with superuser privileges.
-# Verifies at least one argument (username) is provided. If not, it displays usage instructions and exits.
-# Assigns the first argument to the USERNAME variable and collects any additional arguments as COMMENT.
-# Generates a random password for the user.
-# Creates the user account with the specified comment.
-# If the user account creation is successful, it sets the generated password for the user.
-# If setting the password is successful, it displays user information, including the username, comment, generated password, and hostname.
-# It forces the user to change the password on the first login.
-# If the user account creation fails, it displays an error message.
+# This script creates a new user on the local system.
+# You must apply a username as an argument to the script.
+# Optionally, you can also provide a comment for the account as an argument.
+# A password will be automatically generated for the account.
+# The username, password, and host for the account will be displayed.
 
-# Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" != "0" ]]
+# Make sure the script is run with superuser privileges.
+if [[ "${UID}" -ne 0 ]]
 then
-    echo "This script must be run with superuser privileges."
+    echo "Please run with sudo or as root." >&2
     exit 1
 fi
 
-# If the user does not supply at least one argument, then give them help.
-if [[ $# -lt 1 ]]
+# If the user doesn't supply at least one argument, then give them help.
+if [[ "${#}" -lt 1 ]]
 then
-    echo "Usage: $0 username1 [username2, username3 ...]"
-    echo "Creates user accounts for the given usernames."
+    echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
+    echo "Create an account on the local system with the name of USER_NAME and a commends field of COMMEND." >&2
     exit 1
 fi
 
-# The first parameter is the user name.
-USERNAME="$1"
+# The first parameter is the username.
+USER_NAME="${1}"
+
+# The rest of the parameters are comments for the account.
 shift
+COMMENT="${@}"
 
-# The rest of the parameters are for the account comments.
-COMMENT="$@"
+#Generate a password.
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
 
-# Generate a password.
-PASSWORD=$(date +%s%N | sha256sum | head -c 12)
+# Check if the user already exists.
+if id "${USER_NAME}" &>/dev/null; then
+    echo "User ${USER_NAME} already exists." >&2
+    exit 1
+fi
 
-# Create the users with the password.
-useradd -c "$COMMENT" -m "$USERNAME"
+#Create a user with the password.
+useradd -c "${COMMENT}" -m "${USER_NAME}" 2>&1
 
 # Check to see if the useradd command succeeded.
-if [[ "${?}" -eq 0 ]]
+# We don't want to tell the user that an account was created when it was not.
+if [[ "${?}" -ne 0 ]]
 then
+    echo "The account could not be created." >&2
+    exit 1
+fi
 
 # Set the password.
-    echo "$USERNAME:$PASSWORD" | chpasswd
+echo "${USER_NAME}:${PASSWORD}" | chpasswd &> /dev/null
 
-# Check yo see if the passwd command succeeded.
-    if [[ "${?}" -eq 0 ]]
-    then
+# Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+    echo "The password could not be set." >&2
+    exit 1
+fi
+
+# Force password change on first login.
+passwd -e "${USER_NAME}" &> /dev/null
 
 # Display the username, password, and the host where the user was created.
-        echo
-        echo "User: ${USERNAME}"
-        echo "Comment: ${COMMENT}"
-        echo "Password: ${PASSWORD}"
-        echo "Hostname: $(hostname)"
-
-        # Force password on first login.
-        passwd -e "$USERNAME"
-    fi
-elif [[ "${?}" -eq 1 ]]
-then
-    echo "Failed to create user."
-
-fi
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
+echo
+echo 'host:'
+echo "${HOSTNAME}"
+exit 0
